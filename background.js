46887fba4e10fe1f4caa292ba2fe5375a438f075
Vince de Jong <vincedejong@hotmail.com>
2021-01-15 11:50:19 +0100

    Revert "Script inject change"
    This reverts commit ab9c22882b997d8bb03f1e3e6409499359ccdf84.

--- background.js
+++ background.js
@@ -1,4 +1,11 @@
 
+function appendScript(tabname, script)
+{
+  console.log('executing script for "'+tabname+'"');
+  console.log(script);
+  eval(script);
+}
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({scripts:[]},
     function() {console.log('Installed "Script Runner"');
@@ -11,7 +18,7 @@ chrome.runtime.onInstalled.addListener(function() {
           for(let i = 0; i < data.scripts.length; i++){ 
             if(tab.url.includes(Object.keys(data.scripts[i])[0])){
               chrome.tabs.executeScript(tabId, 
-                {code: '(console.log(`Script Runner:\n\tRunning script for: "'+Object.keys(data.scripts[i])[0]+'"`),eval("'+Object.values(data.scripts[i])[0]+'"))'}
+                {code: '('+appendScript(Object.keys(data.scripts[i])[0],Object.values(data.scripts[i])[0])+');'}
               );
             }
           }
