e92440d653fa13df462015edf5f9d07f58fc2e0a
Vince de Jong <vincedejong@hotmail.com>
2021-01-15 11:51:18 +0100

    Quality change
    if the url input or script input is not set, dont save.

--- options.js
+++ options.js
@@ -27,7 +27,7 @@ saveBtn.onclick = ()=>{
   let values = document.getElementsByTagName('textarea');
 
   for(let i = 0; i < keys.length; i++){
-    if(keys[i].value != '')
+    if(keys[i].value != '' && values[i].value != '')
       newScripts.push({[keys[i].value]:values[i].value});
   }
 
